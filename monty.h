9c51aa21ab70dd36c701bb59051bdb96aea18819
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-12-14 14:26:49 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -57,14 +57,14 @@ typedef struct fvar_s
 	int fque;
 }  fvar_t;
 
-/* extern fvar_t fuse;*/
+extern fvar_t fuse;
 
 int fexe(char *, stack_t **, unsigned int, FILE *);
 void free_fstack(stack_t *);
 char *_realloc(char *ptr, unsigned int old_size, unsigned int new_size);
 ssize_t getstdin(char **lineptr, int file);
 char  *clean_line(char *content);
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+/*ssize_t _getline(char **lineptr, size_t *n, FILE *stream);*/
 
 /* basic operations */
 void s_push(stack_t **, unsigned int);
