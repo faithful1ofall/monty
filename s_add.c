fa90d6dd1a9b7f221ab9a63b90a3221adbb36365
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-12-14 12:22:27 +0100

    Create s_add.c

---
+++ s_add.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * s_add - adds the top two elements of the stack.
+ * @fhead: stack fhead
+ * @counter: line_number
+ * Return: no return
+*/
+void s_add(stack_t **fhead, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, faux;
+
+	h = *fhead;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(fuse.file);
+		free(fuse.fcontent);
+		free_fstack(*fhead);
+		exit(EXIT_FAILURE);
+	}
+	h = *fhead;
+	faux = h->n + h->next->n;
+	h->next->n = faux;
+	*fhead = h->next;
+	free(h);
+}
