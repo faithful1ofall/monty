96a3b1835b59dec3a318ed58c124c8bc59605f6c
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2023-12-14 12:53:40 +0100

    Update monty.c

--- monty.c
+++ monty.c
@@ -1,54 +1,59 @@
 #include "monty.h"
 
+/**
+* _getline - custom getline function
+* @lineptr: content and lines
+* @n: the byte size
+* @stream: the file stream
+* Return: 0 on success
+*/
 
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream) {
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
+{
 	size_t pos;
 	int ch;
 	char *new_ptr;
 
-    if (lineptr == NULL || n == NULL || stream == NULL) {
-        return -1;
-    }
+    if (lineptr == NULL || n == NULL || stream == NULL)
+	    return (-1);
 
     if (*lineptr == NULL || *n == 0) {
         *n = INITIAL_BUFFER_SIZE;
         *lineptr = (char *)malloc(*n);
 
-        if (*lineptr == NULL) {
-            return -1;
-        }
+        if (*lineptr == NULL)
+		return (-1);
     }
 
     pos = 0;
 
     while ((ch = fgetc(stream)) != EOF) {
-        if (pos >= *n - 1) {
+        if (pos >= *n - 1)
+	{
             *n *= 2;
             new_ptr = (char *)realloc(*lineptr, *n);
 
-            if (new_ptr == NULL) {
-                return -1;
-            }
+            if (new_ptr == NULL)
+		    return (-1);
 
             *lineptr = new_ptr;
         }
 
         (*lineptr)[pos++] = ch;
 
-        if (ch == '\n') {
-            break;
-        }
+        if (ch == '\n')
+		break;
     }
 
-    if (pos == 0) {
-        return -1;
-    }
+    if (pos == 0)
+	    return (-1);
 
     (*lineptr)[pos] = '\0';
-    return pos;
+    return (pos);
 }
 
 fvar_t fuse = {NULL, NULL, NULL, 0};
+
 /**
 * main - monty code interpreter
 * @ac: number of arguments
